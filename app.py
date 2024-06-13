3cbbdb26e3767f4899f1d8a673f00b740a4f7c73
GKK <benda1989@live.com>
2024-06-13 18:27:33 +0800

    init

---
+++ app.py
@@ -0,0 +1,125 @@
+# pip install pydocx python-docx flask
+from docx2html import docx2html
+from flask import Flask, request, jsonify, Response, g, send_file
+import sqlite3
+import hashlib
+import time
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['HTML_FOLDER'] = 'htmls/'
+app.config['DOCX_FOLDER'] = 'results/'
+app.config['ALLOWED_EXTENSIONS'] = {'docx'}
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect("db")
+    return db
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        db.cursor().executescript('''CREATE TABLE IF NOT EXISTS datas(
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT NOT NULL,
+                            md5 TEXT NOT NULL,
+                            datas TEXT NOT NULL,
+                            docx  bool default false,
+                            createdAt timestamp default (datetime('now','localtime'))
+                            );
+                            CREATE INDEX md5_index ON datas(md5);''')
+        db.commit()
+
+
+def save_db(sql, args=()):
+    with app.app_context():
+        db = get_db()
+        db.cursor().execute(sql, args)
+        db.commit()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+def getOne(md5):
+    return query_db('select * from datas where md5 = ?', (md5,), one=True)
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/html/<id>', methods=['GET'])
+def html(id):
+    return Response(open(app.config['HTML_FOLDER'] + id).read(), mimetype='text/html')
+
+
+@app.route('/docx/<id>', methods=["GET", 'POST'])
+def create_docx(id):
+
+    one = getOne(id)
+    if one:
+        fp = app.config['DOCX_FOLDER'] + id + ".docx"
+        if request.method == "GET" and not one[4]:
+            return jsonify({'msg': 'No file data'})
+        if request.method == "POST":
+            data = request.get_json()
+            print(data)
+            if "datas" not in data:
+                return jsonify({'msg': 'should has datas'})
+            docx2html(app.config['UPLOAD_FOLDER'] + one[1]).save(fp, data.get("datas"), data.get("table", []))
+            save_db("update  datas set docx=true where md5 =?", (id,))
+        return send_file(fp, as_attachment=True)
+    else:
+        return jsonify({'msg': 'No such file'})
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'msg': 'No file part'})
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'msg': 'No selected file'})
+    if file and allowed_file(file.filename):
+        fn = str(time.time())+file.filename
+        fp = app.config['UPLOAD_FOLDER']+fn
+        md5, datas = "", []
+        with open(fp, "wb") as fs:
+            file.save(fs)
+        with open(fp, "rb") as fs:
+            md5 = hashlib.md5(fs.read()).hexdigest()
+        one = getOne(md5)
+        if not one:
+            docs = docx2html(fp)
+            with open(app.config['HTML_FOLDER']+md5, "w", encoding="utf-8") as f:
+                f.write(docs.export())
+            datas = docs.inputDatas
+            save_db("insert into datas(name,md5,datas)  values (?, ?, ?)",  (fn, md5, str(datas)))
+        else:
+            os.remove(fp)
+            datas = one[3]
+        return jsonify({'id': md5,
+                        "datas": datas})
+    else:
+        return jsonify({'msg': 'File type not allowed'})
+
+
+if __name__ == "__main__":
+    # init_db()
+    app.run()
---
+++ docx2html.py
@@ -0,0 +1,287 @@
+# pip install pydocx python-docx
+import time
+import itertools
+from PIL import Image
+from docx import Document
+from pydocx.export.html import PyDocXHTMLExporter, HtmlTag
+from itertools import chain
+
+
+jss = '''
+        document.addEventListener('DOMContentLoaded', function () {
+            let dataForms = document.getElementsByTagName('form')
+            for (let i = 0; i < dataForms.length; i++) {
+                dataForms[i].addEventListener('submit', function (e) {
+                    e.preventDefault();
+                    addRow(i, dataForms[i].getElementsByTagName("input").length);
+                });
+            }
+            document.querySelectorAll('tbody tr td').forEach(function (cell) {
+                if (cell.innerHTML.length === 0) {
+                    cell.addEventListener('dblclick', function () {
+                        makeEditable(cell);
+                    });
+                }
+            });
+            document.addEventListener('input', function (e) {
+                e.target.style.width = e.target.scrollWidth + 'px';
+            });
+            document.addEventListener('keydown', function (e) {
+                if (e.key === 'Enter') {
+                    if (e.target.tagName === "INPUT") {
+                        nextInput(e.target)
+                    }
+                }
+            });
+            window.addEventListener('message', function (e) {
+                if (e.data === "save") {
+                    e.source.postMessage(inputData(), '*');
+                } else {
+                    let datas = e.data.inputs;
+                    let tables = e.data.tables
+                    let inputs = document.getElementsByClassName("pydocx-underline");
+                    inputs[0].focus();
+                    if (datas.length === inputs.length) {
+                        for (let i = 0; i < inputs.length; i++) {
+                            inputs[i].type = datas[i].type
+                            inputs[i].value = datas[i].value
+                        }
+                    }
+                    let tbodys = document.getElementsByTagName("tbody");
+                    if (tables.length === tbodys.length) {
+                        for (let i = 0; i < tables.length; i++) {
+                            let rcount = tbodys[i].childElementCount;
+                            for (let j = 0; j < tables[i].length; j++) {
+                                if (j < rcount) {
+                                    for (let k = 0; k < tables[i][j].length; k++) {
+                                        tbodys[i].children[j].cells[k].innerHTML = tables[i][j][k]
+                                    }
+                                } else {
+                                    let row = tbodys[i].insertRow();
+                                    for (let k = 0; k < tables[i][j].length; k++) {
+                                        let cell = row.insertCell(k);
+                                        cell.textContent = tables[i][j][k]
+                                        makeEditable(cell);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        });
+        function nextInput(thisInput) {
+            let inputs = document.getElementsByClassName("pydocx-underline");
+            for (let i = 0; i < inputs.length+1; i++) {
+                if (thisInput == inputs[i]) {
+                    if (i == inputs.length - 1) {
+                        inputs[i].focus();
+                    } else {
+                        inputs[i + 1].focus();
+                    }
+                    break;
+                }
+                if (i === inputs.length){ 
+                    inputs[0].focus();
+                }
+            }
+        };
+        function inputData() {
+            let inputs = [];
+            Array.from(document.getElementsByClassName("pydocx-underline")).forEach(element => {
+                inputs.push(element.value)
+            });
+            let tables = [];
+            let tbodys = document.getElementsByTagName("tbody");
+            for (let k = 0; k < tbodys.length; k++) {
+                let tbody = [];
+                for (let i = 0; i < tbodys[k].childElementCount; i++) {
+                    let row = tbodys[0].children[i];
+                    let rows = [];
+                    for (let j = 0; j < row.childElementCount; j++) {
+                        rows.push(row.cells[j].innerHTML);
+                    }
+                    tbody.push(rows);
+                }
+                tables.push(tbody);
+            }
+            return { inputs, tables }
+        };
+        function addRow(i, n) {
+            let tableBody = document.getElementsByTagName('tbody')[i];
+            let row = tableBody.insertRow();
+            for (let i = 0; i < n; i++) {
+                let cell = row.insertCell(i);
+                cell.textContent = document.getElementById('form' + (i + 1)).value;
+                makeEditable(cell);
+            }
+            let dataForms = document.getElementsByTagName('form')
+            for (let i = 0; i < dataForms.length; i++) {
+                dataForms[i].reset();
+            }
+        };
+        function makeEditable(cell) {
+            cell.contentEditable = true;
+            cell.focus();
+        };
+'''
+
+
+class item():
+    data = None
+    required = True
+    date = None
+    table = None
+
+    def __init__(self, detail="") -> None:
+        self.detail = detail
+
+
+class docx2html(PyDocXHTMLExporter):
+    colums = []
+    inputDatas = []
+    inputData = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def head(self):
+        tag = HtmlTag('head')
+        results = chain(self.meta(), self.style(), self.js())
+        return tag.apply(results)
+
+    def js(self):
+        tag = HtmlTag('script')
+        return tag.apply(jss)
+
+    # 表格
+    def get_table_tag(self, table):
+        return HtmlTag('table', **{
+            'width': '100%',
+            "border": '1',
+        })
+
+    def export_table(self, table):
+        results = list(itertools.chain.from_iterable([super().export_table(table)]))
+        for i, result in enumerate(results):
+            if i == 1:
+                yield '<thead>'
+            if len(results) == i+2:
+                yield '</tbody>'
+            else:
+                yield result
+
+        yield '<form class="dataForm">'
+        for i, _ in enumerate(self.colums[0]):
+            yield '<input id="form%d" class="table-input"></input>' % (i+1,)
+        yield '<button type="submit">添加</button>'
+        yield '</form>'
+
+    def export_table_row(self, table_row):
+        colums = []
+        results = super(PyDocXHTMLExporter, self).export_table_row(table_row)
+        res = []
+        for result in results:
+            if isinstance(result, str):
+                colums.append(result)
+            res.append(result)
+        if colums:
+            i = 0
+            for v in res:
+                if isinstance(v, HtmlTag):
+                    if v.tag == "td":
+                        if not v.closed:
+                            i += 1
+                            v.attrs["field"] = 'form%d' % i
+                        yield v
+                else:
+                    yield v
+            if colums not in self.colums:
+                self.colums.append(colums)
+            if len(colums) == len(table_row.cells):
+                yield '</thead><tbody>'
+            else:
+                yield '</tr>'
+            yield '<tr>'
+
+    # 输入
+    def export_run_property_underline(self, run, results):
+        self.reset_data()
+        attrs = {
+            'class': 'pydocx-underline',
+        }
+        tag = HtmlTag('input', **attrs)
+        return tag.apply(results)
+
+    def reset_data(self):
+        if len(self.inputData) > 1:
+            data = self.inputData[-1]
+            if len(data) < 2 and len(self.inputData) > 2:
+                data = self.inputData[-2] + data
+            self.inputDatas.append(data)
+            self.inputData = []
+        elif len(self.inputDatas) > 0:
+            self.inputDatas.append("")
+
+    # 图片缩放
+    def get_image_tag(self, image, width=None, height=None, rotate=None):
+        attrs = {
+            'width': width,
+            'height': height
+        }
+        c = rotate and rotate in (270, 90)
+        ic = Image.open(image.stream)
+        if (ic.width <= 595 and not c) or (ic.height <= 842 and c):
+            image_src = self.get_image_source(image)
+            if rotate:
+                attrs['style'] = 'transform: rotate(%sdeg);' % rotate
+        else:
+            import base64
+            from io import BytesIO
+            if c:
+                ic = ic.rotate(360-rotate, expand=True)
+                attrs["width"], attrs["height"] = height, width
+            ic = ic.resize((842, int(ic.height * 842 / ic.width)))
+            buffer = BytesIO()
+            ic.save(buffer, format="JPEG")
+            image_src = 'data:image/JPEG;base64,{data}'.format(data=base64.b64encode(buffer.getvalue()).decode('utf-8'))
+        if image_src:
+            return HtmlTag(
+                'img',
+                allow_self_closing=True,
+                allow_whitespace=True,
+                src=image_src,
+                **attrs
+            )
+
+    # 导出html
+    def export(self):
+        re = []
+        for result in super(PyDocXHTMLExporter, self).export():
+            if isinstance(result, HtmlTag):
+                re.append(result.to_html())
+            else:
+                self.inputData.append(result)
+                re.append(result)
+        return ''.join(re)
+
+    # 模版数据替换
+    def save(self, paths="", datas=[], tables=[]):
+        paths = paths or self.path.replace(".docx", "_%d.docx" % time.time())
+        doc, i = Document(self.path), 0
+        for para in doc.paragraphs:
+            for run in para.runs:
+                if run.underline:
+                    run.text = datas[i]
+                    i += 1
+        for i, table in enumerate(tables):
+            rn = len(table.rows)-1  # 第一行标题
+            while rn < len(table):
+                doc.tables[i].add_row()
+                rn += 1
+            for j, row in enumerate(doc.tables[i].rows):
+                if j > 0:
+                    for k, cell in enumerate(row.cells):
+                        cell.text = table[j-1][k]
+        doc.save(paths)
+        return paths
